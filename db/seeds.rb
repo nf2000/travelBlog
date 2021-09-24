c825cee76a2f123b198b77614fff35980508337e
nf2000 <nimraf@chillibean.com>
2021-09-24 12:08:26 +0100

    fixed the layout for the title label and title name

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p1 = Post.create(title: "Techr", author: "Chris Kyle", category: "Tech", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+p2 = Post.create(title: "Air", author: "Steven jake", category: "Space", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+p3 = Post.create(title: "Travelling Europe", author: "jones", category: "travelling", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
