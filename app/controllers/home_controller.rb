03f1752ac24dc5d8181c0b8cc803b2dcc4d88a55
nf2000 <nimraf@chillibean.com>
2021-09-24 16:45:45 +0100

    in progress of adding the create post functionality - added controller actions and created new.html view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,21 @@ class HomeController < ApplicationController
     def index
         @posts = Post.all
     end
-
+    def new
+        @post = Post.new
+    end
+  
+    def create
+    @post = Post.new(post_param)
+      if @post.save
+        redirect_to '/home'
+      else
+        render 'new'
+        end
+    end
+  
+    private
+    def post_param
+    params.require(:post).permit(:title, :author, :category, :content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :home
+  resources :home, :post
 
-  get '/home/index' => 'home#index'
+  root 'home#index'
+  get '/home/new' => 'home#new'
+ 
 end
