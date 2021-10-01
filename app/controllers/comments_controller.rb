1b5162744e0d8e63d9358fc3077bb7da17fa7079
nf2000 <nimraf@chillibean.com>
2021-10-01 17:33:19 +0100

    added new and create actions inside comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,25 @@
 class CommentsController < ApplicationController
 
-   
+    def new
+        @comment =  Comment.new
+    end
+  
+    def create
+        @comment = Comment.new(comment_param)  
+        post_is_valid = post.valid? 
+        if post_is_valid  
+            post.save
+           return redirect_to posts_path
+
+        else
+            flash.now[:alert] = "Unable to create a comment"
+            render 'new'
+        end  
+       
+    end
+
+    private
+    def comment_param
+    params.require(:comment).permit(:comment)
+    end
 end
