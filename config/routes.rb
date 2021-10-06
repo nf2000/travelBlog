cea0cba088bf5de3dda0873dc29ad0e21402821a
nf2000 <nimraf@chillibean.com>
2021-10-06 14:56:05 +0100

    put in nested resources since there is an assocaition between posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :posts , :comments
+  resources :posts do
+    resources :comments  #nested resources 
+  end
 
   root 'posts#index'
-  
-
  
 end
