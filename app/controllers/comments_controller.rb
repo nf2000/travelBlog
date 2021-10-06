b8ee718f34870a24c88a9bc7bc1496115475cd3c
nf2000 <nimraf@chillibean.com>
2021-10-06 14:41:04 +0100

    added a intialisation for comment post id to be the same as the post objetct's id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,22 @@
 class CommentsController < ApplicationController
-
+    before_action :find_post
     def index
         @comments = Comment.all
     end
 
     def new
-
         @comment = Comment.new
-
     end
 
   
     def create
-        comment = Comment.new(comment_param)  
-        comment_is_valid = comment.valid? 
+        @comment = Comment.new(comment_param)  
+        @comment.post_id = @post.id
+        comment_is_valid = @comment.valid? 
         if comment_is_valid
-            comment.save
+            @comment.save
             #flash[:notice] =  "Comment successfully created." 
-            return redirect_to post_path("#{comment_param[:post_id]}"), success: "Comment successfully created."
+            return redirect_to post_path("#{@comment.post_id}"), success: "Comment successfully created."
 
         else
             flash.now[:danger] = "comment was not created"
@@ -30,5 +29,10 @@ class CommentsController < ApplicationController
     private
     def comment_param
     params.require(:comment).permit(:comment,:post_id)
+    end
+
+    private 
+    def find_post
+        @post = Post.find(params[:post_id])
     end
 end
